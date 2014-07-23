2611d2cab4c1f5391833d1fcf5ebac146c686c98
UWM8 <ufevuwruce83832@mailcatch.com>
2014-07-23 18:50:55 -0400

    Update Hero_Lich_Auto.lua

--- Scripts/Hero_Lich_Auto.lua
+++ Scripts/Hero_Lich_Auto.lua
@@ -11,7 +11,7 @@ local disabl      = false
 local monitor     = client.screenSize.x/1600
 local F15         = drawMgr:CreateFont("F15","Tahoma",15*monitor,550*monitor)
 local F14         = drawMgr:CreateFont("F14","Tahoma",14*monitor,550*monitor) 
-local statusText  = drawMgr:CreateText(10*monitor,500*monitor,-1,"(" .. string.char(toggleKey) .. ") Auto Lich: Off",F14)
+local statusText  = drawMgr:CreateText(10*monitor,500*monitor,-1,"(" .. string.char(toggleKey) .. ") Auto Lich: Off",F14) statusText.visible = false
 
 local hotkeyText
 if string.byte("A") <= toggleKey and toggleKey <= string.byte("Z") then
@@ -45,7 +45,7 @@ function Tick(tick)
 		local creeps     = entityList:GetEntities({classId=CDOTA_BaseNPC_Creep_Lane, alive=true, visible=true, team=me.team})
 
 		for i,v in ipairs(allies) do
-			if GetDistance2D(v,me) <= icearmor.castRange and activ and v.name ~= me.name and icearmor:CanBeCasted() and icearmor and v:DoesHaveModifier("modifier_lich_frost_armor") == false and v.alive and v.visible and not me.abilityPhase then
+			if GetDistance2D(v,me) <= icearmor.castRange + 15 and activ and v.name ~= me.name and icearmor:CanBeCasted() and icearmor and v:DoesHaveModifier("modifier_lich_frost_armor") == false and v.alive and v.visible and not me.abilityPhase then
 				me:SafeCastAbility(icearmor,v)
 				Sleep(500)
 				break
@@ -53,7 +53,7 @@ function Tick(tick)
 		end
 
 		for i,v in ipairs(creeps) do
-			if GetDistance2D(v,me) <= sacrifice.castRange and activ and sacrifice and sacrifice:CanBeCasted() and v.health == v.maxHealth and not v:IsRanged() and v.spawned and v.alive and v.visible and not me.abilityPhase then
+			if GetDistance2D(v,me) <= sacrifice.castRange + 15 and activ and sacrifice and sacrifice:CanBeCasted() and v.health == v.maxHealth and not v:IsRanged() and v.spawned and v.alive and v.visible and not me.abilityPhase then
 				me:SafeCastAbility(sacrifice,v)
 				Sleep(500)
 				break
@@ -70,12 +70,14 @@ function Load()
 			statusText.text = ""
 			script:Disable() 
 		else
+			statusText.visible = true
+
 			reg = true
 			script:RegisterEvent(EVENT_TICK,Tick)
 			script:RegisterEvent(EVENT_KEY,Key)
 			script:UnregisterEvent(Load)
 		end
-	end
+
 end
 
 function GameClose()
