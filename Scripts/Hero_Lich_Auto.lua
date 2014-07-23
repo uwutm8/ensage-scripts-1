36110ae9a133a3256254a0ce5f05704d3e65f1cf
UWM8 <ufevuwruce83832@mailcatch.com>
2014-07-23 05:42:06 -0400

    Update Hero_Lich_Auto.lua

--- Scripts/Hero_Lich_Auto.lua
+++ Scripts/Hero_Lich_Auto.lua
@@ -2,47 +2,40 @@ require("libs.Utils")
 require("libs.ScriptConfig")
 config = ScriptConfig.new()
 config:SetParameter("ToggleKey", "G", config.TYPE_HOTKEY)
-config:SetParameter("UseDisableKey", true)
 config:Load()
 
 local toggleKey   = config.ToggleKey
-local disableKey  = config.UseDisableKey
 local reg         = false
 local activ		  = false
 local disabl      = false
 local monitor     = client.screenSize.x/1600
 local F15         = drawMgr:CreateFont("F15","Tahoma",15*monitor,550*monitor)
 local F14         = drawMgr:CreateFont("F14","Tahoma",14*monitor,550*monitor) 
-local statusText  = drawMgr:CreateText(10*monitor,530*monitor,-1,"(" .. string.char(toggleKey) .. ") Auto Lich: Off",F14)
-local statusText2 = drawMgr:CreateText(10*monitor,530*monitor,-1,"(" .. string.char(toggleKey) .. ") Auto Lich: On",F14)
+local statusText  = drawMgr:CreateText(10*monitor,500*monitor,-1,"(" .. string.char(toggleKey) .. ") Auto Lich: Off",F14)
+
+local hotkeyText
+if string.byte("A") <= toggleKey and toggleKey <= string.byte("Z") then
+	hotkeyText = string.char(toggleKey)
+else
+	hotkeyText = ""..toggleKey
+end
 
 function Key(msg,code)
-	if client.chat or client.console then return end
+	if client.chat or client.console or client.loading then return end
 	if IsKeyDown(toggleKey) then
 		activ = not activ
+		if activ then
+			statusText.text = "(" .. hotkeyText .. ") Auto Lich: On"
+		else
+			statusText.text = "(" .. hotkeyText .. ") Auto Lich: Off"
+		end
 	end
 end
 
 function Tick(tick)
 	if not SleepCheck() then return end	Sleep(125)
 	local me = entityList:GetMyHero()
-	if not me then return end
-
-	local DisableKey = 0x20
-
-	if IsKeyDown(DisableKey) and disableKey then
-		disabl = true
-	else
-		disabl = false
-	end
-
-	if disabl or not activ then
-		statusText2.visible = false
-		statusText.visible  = true
-	else
-		statusText.visible  = false
-		statusText2.visible = true
-	end
+	if not (me or activ) then return end
 
 	if me.alive and not me:IsChanneling() then
 		local icearmor   = me:GetAbility(2)
@@ -70,11 +63,11 @@ function Tick(tick)
 end
 
 function Load()
+
 	if PlayingGame() then
 		local me = entityList:GetMyHero()
 		if me.classId ~= CDOTA_Unit_Hero_Lich then 
-			statusText.visible  = false
-			statusText2.visible = false
+			statusText.text = ""
 			script:Disable() 
 		else
 			reg = true
