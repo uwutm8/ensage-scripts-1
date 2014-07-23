217decca833738d873eab52609655eea3f8a8a45
UWM8 <ufevuwruce83832@mailcatch.com>
2014-07-23 19:26:58 -0400

    Update Hero_Disruptor_Auto.lua

--- Scripts/Hero_Disruptor_Auto.lua
+++ Scripts/Hero_Disruptor_Auto.lua
@@ -2,24 +2,25 @@ require("libs.Utils")
 require("libs.ScriptConfig")
 config = ScriptConfig.new()
 config:SetParameter("ToggleKey", "G", config.TYPE_HOTKEY)
-config:SetParameter("UseDisableKey", true)
 config:Load()
 
 local toggleKey   = config.ToggleKey
-local disableKey  = config.UseDisableKey
 local reg         = false
 local activ       = true
-local disabl      = false
 local monitor     = client.screenSize.x/1600
 local F15         = drawMgr:CreateFont("F15","Tahoma",15*monitor,550*monitor)
 local F14         = drawMgr:CreateFont("F14","Tahoma",14*monitor,550*monitor) 
-local statusText  = drawMgr:CreateText(10*monitor,530*monitor,-1,"(" .. string.char(toggleKey) .. ") Auto Disruptor: Off",F14)
-local statusText2 = drawMgr:CreateText(10*monitor,530*monitor,-1,"(" .. string.char(toggleKey) .. ") Auto Disruptor: On",F14)
+local statusText  = drawMgr:CreateText(10*monitor,530*monitor,-1,"(" .. string.char(toggleKey) .. ") Auto Disruptor: Off",F14) statusText.visible = false
 
 function Key(msg,code)
 	if client.chat or client.console then return end
 	if IsKeyDown(toggleKey) then
 		activ = not activ
+		if activ then
+			statusText.text = "(" .. hotkeyText .. ") Auto Disruptor: On"
+		else
+			statusText.text = "(" .. hotkeyText .. ") Auto Disruptor: Off"
+		end
 	end
 end
 
@@ -27,22 +28,6 @@ function Tick(tick)
 	if not SleepCheck() then return end	Sleep(125)
 	local me = entityList:GetMyHero()
 	if not me then return end
-
-	local DisableKey = 0x20
-
-	if IsKeyDown(DisableKey) and disableKey then
-		disabl = true
-	else
-		disabl = false
-	end
-
-	if disabl or not activ then
-		statusText2.visible = false
-		statusText.visible  = true
-	else
-		statusText.visible  = false
-		statusText2.visible = true
-	end
 
 	if me.alive and not me:IsChanneling() then
 		local glimpse = me:GetAbility(2)
@@ -87,26 +72,28 @@ function Load()
 	if PlayingGame() then
 		local me = entityList:GetMyHero()
 		if me.classId ~= CDOTA_Unit_Hero_Disruptor then 
-			statusText.visible  = false
-			statusText2.visible = false
-			script:Disable() 
+			script:Disable()
 		else
+			statusText.visible = true
 			reg = true
 			script:RegisterEvent(EVENT_TICK,Tick)
 			script:RegisterEvent(EVENT_KEY,Key)
 			script:UnregisterEvent(Load)
 		end
+
+
 	end
 end
 
 function GameClose()
+	collectgarbage("collect")
 	if reg then
 		script:UnregisterEvent(Tick)
 		script:UnregisterEvent(Key)
 		script:RegisterEvent(EVENT_TICK,Load)
 		reg = false
+		statusText.visible = false
 	end
-	collectgarbage("collect")
 end
 
 script:RegisterEvent(EVENT_CLOSE,GameClose)
