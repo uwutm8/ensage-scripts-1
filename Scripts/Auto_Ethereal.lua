f5d5984600a3b9682938337ee82a6dd8aecf0683
UWM8 <ufevuwruce83832@mailcatch.com>
2014-07-28 11:27:25 -0400

    Create Auto_Ethereal.lua

---
+++ Scripts/Auto_Ethereal.lua
@@ -0,0 +1,82 @@
+require("libs.Utils")
+require("libs.ScriptConfig")
+config = ScriptConfig.new()
+config:SetParameter("Active", "N", config.TYPE_HOTKEY)
+config:Load()
+
+local toggleKey   = config.Active
+local disableKey  = config.UseDisableKey
+local reg         = false
+local activ       = true
+local monitor     = client.screenSize.x/1600
+local F15         = drawMgr:CreateFont("F15","Tahoma",15*monitor,550*monitor)
+local F14         = drawMgr:CreateFont("F14","Tahoma",14*monitor,550*monitor) 
+local statusText  = drawMgr:CreateText(10*monitor,245*monitor,-1,"(" .. string.char(toggleKey) .. ") Auto Ethereal: On",F14) statusText.visible = false
+
+local hotkeyText
+if string.byte("A") <= toggleKey and toggleKey <= string.byte("Z") then
+	hotkeyText = string.char(toggleKey)
+else
+	hotkeyText = ""..toggleKey
+end
+
+function Key(msg,code)
+	if client.chat or client.console or client.loading then return end
+	if IsKeyDown(toggleKey) then
+		activ = not activ
+		if activ then
+			statusText.text = "(" .. hotkeyText .. ") Auto Ethereal: On"
+		else
+			statusText.text = "(" .. hotkeyText .. ") Auto Ethereal: Off"
+		end
+	end
+end
+
+function Tick(tick)
+	if not SleepCheck() then return end	Sleep(30)
+	local me = entityList:GetMyHero()
+	if not (me and activ) then return end
+	if me.alive and not me:IsChanneling() then
+		local eth     = me:FindItem("item_ethereal_blade")
+		if eth then
+			statusText.visible = true
+		end
+		local enemies = entityList:GetEntities({type=LuaEntity.TYPE_HERO,team = 5-me.team,alive=true,visible=true,illusion=false})
+		for i,v in ipairs(enemies) do
+			local MI = v:IsMagicImmune()
+			if GetDistance2D(v,me) <= 800 and eth and eth:CanBeCasted() and activ and not MI then
+				me:SafeCastItem("item_ethereal_blade",v)
+				Sleep(500)
+				break
+			end
+		end
+	end
+end
+
+function Load()
+	if PlayingGame() then
+		local me = entityList:GetMyHero()
+		if not me then
+			script:Disable()
+		else
+			reg = true
+			script:RegisterEvent(EVENT_TICK,Tick)
+			script:RegisterEvent(EVENT_KEY,Key)
+			script:UnregisterEvent(Load)
+		end
+	end
+end
+
+function GameClose()
+	collectgarbage("collect")
+	if reg then
+		script:UnregisterEvent(Tick)
+		script:UnregisterEvent(Key)
+		script:RegisterEvent(EVENT_TICK,Load)
+		reg = false
+		statusText.visible = false
+	end
+end
+
+script:RegisterEvent(EVENT_CLOSE,GameClose)
+script:RegisterEvent(EVENT_TICK,Load)
