71e918f78c9748a2119f41c3daa8292be32cbf33
UWM8 <ufevuwruce83832@mailcatch.com>
2014-07-31 03:51:10 -0400

    Create Auto_Aegis.lua

---
+++ Scripts/Auto_Aegis.lua
@@ -0,0 +1,135 @@
+--[[potential steal spell special instructions for death of rosh:
+   earth spirit(?)
+   spectre(Haunt->reality illusion closest to aegis->steal->spectral dagger out)
+   timbersaw(cut trees on cliff, timberchain to other side and steal aegis while you're over it)
+
+   if sniper/rattletrap providing vision, use jugg ult, etc
+	auto spawn veno ward and deny
+   potential deny:
+   lycan(auto deny by invis wolves), broodmother(army of broodmother's spiders), enigma(army of demonic conversions),
+   invoker(spirits),lone druid(bear),naga(illusions),np(nature call),pl(illusions),shadowshaman(serpent ward),
+   sniper(shrapnel, deny),terrorblade(illusions),veno(wards),visage(familiars),warlock(chaotic offering rosh pit, then deny), ]]
+
+
+   require("libs.Utils")
+   require("libs.ScriptConfig")
+
+   config = ScriptConfig.new()
+   config:SetParameter("ToggleKey", "H", config.TYPE_HOTKEY)
+   config:Load()
+
+   local toggleKey   = config.ToggleKey
+   local reg         = false
+   local activ       = false
+   local monitor     = client.screenSize.x/1600
+   local F15         = drawMgr:CreateFont("F15","Tahoma",15*monitor,550*monitor)
+   local F14         = drawMgr:CreateFont("F14","Tahoma",14*monitor,550*monitor) 
+   local statusText  = drawMgr:CreateText(10*monitor,515*monitor,-1,"(" .. string.char(toggleKey) .. ") Auto Aegis: Off",F14) statusText.visible = true
+
+local hotkeyText -- toggleKey might be a keycode number, so string.char will throw an error!!
+if string.byte("A") <= toggleKey and toggleKey <= string.byte("Z") then
+	hotkeyText = string.char(toggleKey)
+else
+	hotkeyText = ""..toggleKey
+end
+
+function Key(msg,code)
+	if client.chat or client.console then return end
+	if IsKeyDown(toggleKey) then
+		activ = not activ
+		if activ then
+			statusText.text = "(" .. hotkeyText .. ") Auto Aegis: On"
+		else
+			statusText.text = "(" .. hotkeyText .. ") Auto Aegis: Off"
+		end
+	end
+end
+
+function Tick(tick)
+	if not SleepCheck() then return end
+	Sleep(15)
+	if PlayingGame() then
+		local me = entityList:GetMyHero()
+		if not (activ or me) then return end
+		if me.alive and not me:IsChanneling() then
+			local items = entityList:GetEntities({type=LuaEntity.TYPE_ITEM_PHYSICAL})
+			for i,v in ipairs(items) do
+				local IH = v.itemHolds
+				if IH.name == "item_aegis" and GetDistance2D(v,me) <= 400 then
+					entityList:GetMyPlayer():TakeItem(v)
+					Sleep(500)
+					break
+				end
+			end
+		end
+	end
+end
+
+function Roshan( kill )
+	if PlayingGame() then
+		local me = entityList:GetMyHero()
+		local aegisloc   = Vector(2413.25,-239.313,4.1875)
+		local sleightloc = Vector(2515,-124,3)
+		local blink      = me:FindItem("item_blink")
+
+		if kill.name == "dota_roshan_kill" and activ then
+			if GetDistance2D(aegisloc,me) <= 1200 and blink and blink.cd == 0 then		
+				me:SafeCastItem(blink.name,aegisloc)
+			elseif me.classId == CDOTA_Unit_Hero_EmberSpirit and GetDistance2D(sleightloc,me) <= 700 then
+				local sleight = me:GetAbility(2)
+				if sleight:CanBeCasted() then
+					me:SafeCastAbility(sleight,sleightloc)
+				end
+			elseif me.classId == CDOTA_Unit_Hero_AntiMage and GetDistance2D(aegisloc,me) <= 1150 then
+				local amblink = me:GetAbility(2)
+				if amblink:CanBeCasted() then
+					me:SafeCastAbility(amblink,aegisloc)
+				end
+			elseif me.classId == CDOTA_Unit_Hero_Rattletrap and GetDistance2D(aegisloc,me) <= 2000 then
+				local hook = me:GetAbility(4)
+				if hook:CanBeCasted() then
+					me:SafeCastAbility(hook,aegisloc)
+				end
+			elseif me.classId == CDOTA_Unit_Hero_FacelessVoid and GetDistance2D(aegisloc,me) <= 1300 then
+				local timewalk = me:GetAbility(1)
+				if timewalk:CanBeCasted() then
+					me:SafeCastAbility(timewalk,aegisloc)
+				end
+			elseif me.classId == CDOTA_Unit_Hero_Magnataur and GetDistance2D(aegisloc,me) <= 1200 then
+				local skewer = me:GetAbility(3)
+				if skewer:CanBeCasted() then
+					me:SafeCastAbility(skewer,aegisloc)
+				end
+			elseif me.classId == CDOTA_Unit_Hero_SandKing and GetDistance2D(aegisloc,me) <= 650 then
+				local burrow = me:GetAbility(1)
+				if burrow:CanBeCasted() then
+					me:SafeCastAbility(burrow,sleightloc)
+				end
+			elseif me.classId == CDOTA_Unit_Hero_QueenOfPain and GetDistance2D(aegisloc,me) <= 1150 then
+				local qopblink = me:GetAbility(2)
+				if qopblink:CanBeCasted() then
+					me:SafeCastAbility(qopblink,aegisloc)
+				end
+			elseif me.classId == CDOTA_Unit_Hero_Morphling and GetDistance2D(aegisloc,me) <= 1000 then
+				local waveform = me:GetAbility(1)
+				if waveform:CanBeCasted() then
+					me:SafeCastAbility(waveform,aegisloc)
+				end
+			elseif me.classId == CDOTA_Unit_Hero_Naga_Siren and GetDistance2D(aegisloc,me) <= 1250 then
+				local sirensong = me:GetAbility(4)
+				if sirensong:CanBeCasted() then
+					me:SafeCastAbility(sirensong,aegisloc)
+				end
+			elseif me.classId == CDOTA_Unit_Hero_StormSpirit and GetDistance2D(aegisloc,me) <= 1000 then
+				local ball = me:GetAbility(4)
+				if ball:CanBeCasted() then
+					me:SafeCastAbility(ball,aegisloc)
+				end
+			end
+		end
+	end
+end
+
+script:RegisterEvent(EVENT_TICK,Tick)
+script:RegisterEvent(EVENT_KEY,Key)
+script:RegisterEvent(EVENT_DOTA,Roshan)
